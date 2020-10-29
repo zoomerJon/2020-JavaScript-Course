afe6d72ca1e32413834ab4bae7bda15fbf0b0f66
zoomerJon <jovalle@nevada.unr.edu>
2020-10-29 16:18:10 -0700

    challenge 4

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -53,5 +53,52 @@ if (markBMI) {
 
 /*************************
  * Coding Challenge 3
+
+
+// Part 1 
+let dolphinsAverage = (97+112+101)/3;
+let koalasAverage = (109+95+106)/3;
+
+// Part 2
+if (dolphinsAverage > koalasAverage) {
+    console.log(`The Dolphins win with an average score of ${dolphinsAverage} points vs ${koalasAverage} 🏆`);
+} else if (koalasAverage > dolphinsAverage) {
+    console.log(`The Koalas win with an average score of ${koalasAverage} points vs ${dolphinsAverage} 🏆`);
+} else {
+    console.log(`Both teams win with the same score (${dolphinsAverage}) 🏆`);
+}
+
+// Bonus 1
+if (dolphinsAverage > koalasAverage && dolphinsAverage >= 100) {
+    console.log(`The Dolphins win with an average score of ${dolphinsAverage} points vs ${koalasAverage} 🏆`);
+} else if (koalasAverage > dolphinsAverage && koalasAverage >= 100) {
+    console.log(`The Koalas win with an average score of ${koalasAverage} points vs ${dolphinsAverage} 🏆`);
+} else if (koalasAverage === dolphinsAverage) {
+    console.log(`Both teams win with the same score (${dolphinsAverage}) 🏆`)
+} else {
+    console.log(`Neither team had the higher score and reached the 100 point minimum. \n\ Dolphins: ${dolphinsAverage} \n\ Koalas: ${koalasAverage}`);
+}
+
+// Bonus 2
+if (dolphinsAverage > koalasAverage && dolphinsAverage >= 100) {
+    console.log(`The Dolphins win with an average score of ${dolphinsAverage} points vs ${koalasAverage} 🏆`);
+} else if (koalasAverage > dolphinsAverage && koalasAverage >= 100) {
+    console.log(`The Koalas win with an average score of ${koalasAverage} points vs ${dolphinsAverage} 🏆`);
+} else if (koalasAverage === dolphinsAverage && koalasAverage >= 100){
+    console.log(`Both teams win with the same score (${dolphinsAverage}) 🏆`);
+} else {
+    console.log(`Neither team reached the 100 point minimum. \n\ Dolphins: ${dolphinsAverage} \n\ Koalas: ${koalasAverage}`);
+}
+*/
+
+/*************************
+ * Coding Challenge 4
  */
 
+// Part 1
+const bill = 275;
+
+const tip = bill >= 50 && bill <= 300 ? .15 * bill : .20 * bill;
+
+// Part 2
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value was ${tip + bill}`);
