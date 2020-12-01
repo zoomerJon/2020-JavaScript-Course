c9224fec19e457049cb2254557fc518d6d3f01f8
zoomerJon <jovalle@nevada.unr.edu>
2020-12-01 01:50:10 -0800

    game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,18 +1,23 @@
 'use strict';
 
-<<<<<<< HEAD
-console.log(document.querySelector('.message').textContent);
-=======
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number!';
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!';
 
-console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value = 23;
-*/
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// console.log(typeof document.querySelector('.message').textContent);
+
+// console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+console.log(secretNumber);
+
+let playerScore = Number(document.querySelector('.score').textContent);
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -21,6 +26,18 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Yeah Baby!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!';
+    loseScore();
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!';
+    loseScore();
   }
 });
->>>>>>> 52431101487dc136d1090dfcb48be6e1d1cb1066
+
+function loseScore() {
+  playerScore--;
+  document.querySelector('.score').textContent = playerScore;
+}
